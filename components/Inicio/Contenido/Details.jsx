1588c4eabaec86f10b29feea5b2007220652b0ff
CDRojasRamirez <cdanielrap@gmail.com>
2024-02-03 21:16:40 -0500

    mejoras push

--- components/Inicio/Contenido/Details.jsx
+++ components/Inicio/Contenido/Details.jsx
@@ -17,7 +17,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import "./GaleriaVIDEO/styles.css";
+// import "./GaleriaVIDEO/styles.css";
 
 // import required modules
 import { Pagination, Navigation } from "swiper/modules";
